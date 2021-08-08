4794d68019bc6d7f18ebec3ce19a6333a4f9388b
Ndjock Michel <ndjockjunior@gmail.com>
2021-08-08 14:54:32 +0100

    fixed bug in admin middleware

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,5 +1,14 @@
-module.exports = function (req, res, next) {
+const { User } = require("../models/User");
+module.exports = async function (req, res, next) {
   //403 stands for forbidden
-  if (req.user.role !== "admin") return res.status(401).send(`Access denied user is a ${req.user.role}!`);
-  next();
+  if (req.user) {
+    try {
+      const user = await User.findOne({ _id: req.user._id });
+      if (user && user.role === "admin") next();
+      else res.status(401).send("Accès refusé");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Une erreur inconnue s'est produite");
+    }
+  }
 };
